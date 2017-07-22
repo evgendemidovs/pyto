c8e360c065617d92a9e9fdf15b5b18b5923a680c
jeka <evgendemidovs@gmail.com>
2017-07-22 08:18:25 +0000

    faf

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
-from myproject import app
-if __name__ == "__main__":
-app.run()
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
